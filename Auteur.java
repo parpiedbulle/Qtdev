9a563f846b8e5eb74aab48d97f443f0b08042653
Marin Chesneau <marin.chsn@gmail.com>
2024-02-01 08:43:18 +0100

    Question 1 javac good

---
+++ Auteur.java
@@ -0,0 +1,16 @@
+public class Auteur {
+    private String nom;
+    private int perfoTraj;
+    private int perfoCom;
+    private int perfoDrame;
+
+    public Auteur() {
+        this.nom = nom;
+        this.perfoTraj = 
+    }
+
+    public getQualiteTragedie() {
+        return this.perfotraj;
+    }
+
+}
